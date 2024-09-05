67fcb3dfa1e8e84ddbfc57d83e45f2efddaafd35
K.Chetan Kumar <chetansmac@KChetans-MacBook-Air.local>
2024-09-05 15:14:25 +0530

    project Structure

---
+++ Client/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, ReactProvider, RouterProvider} from 'react-router-dom';
+import Login from './components/auth/Login.jsx';
+import Register from './components/auth/Register.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"",
+        element:<Login/>
+      },
+      {
+        path:"register",
+        element:<Register/>
+      }
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
