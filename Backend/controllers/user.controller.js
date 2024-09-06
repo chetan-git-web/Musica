c085f81777db4927846b249e01b3aeb2ca023ace
bajajg29 <8721118@gecnilokheri.ac.in>
2024-09-06 12:22:47 +0530

    create server

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+import UserModel from '../models/user.model.js';
+export default class UserController{
+    signUp(req,res){
+    const{name,email,password}=req.body;
+    const user=UserModel.signUp(name,email,password);
+    res.status(201).send(user);   
+}
+
+signIn(req,res){
+    const result=UserModel.signIn(req.body.email,req.body.password);
+   if(!result){
+    return res.status(400).send('Incorrect credentials');
+   }
+else{
+  const token=jwt.sign({
+    userID:result.id,
+    email:result.email,
+  },
+  'k0LYFoM1LLtPX22ogK6oXGxkHeeHJoGS',
+  {
+    expiresIn:'15d',
+  }
+);
+
+return res.status(200).send(token);
+
+}
+}
+
+}
+
+
+
+
+
---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import userRouter from "./routes/user.routes.js";
+//server created
+const server=express();
+server.use(express.json());
+
+server.use('/users', userRouter);
+
+//default request
+server.get('/',(req,res)=>{
+    res.send('HEY!!! HELLO HELLO')
+})
+//port
+server.listen(5000);
+console.log('server is listening on 5000');
+
